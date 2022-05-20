14d3e2c8ef32a48a7a4f5ac14939aa101c317ef6
Artur <a.nurtdinow@yandex.ru>
2022-05-20 03:27:07 +0300

    Upgrade versionCode

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -2,6 +2,6 @@ object ConfigData {
     const val compileSdkVersion = 31
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
-    const val versionCode = 4
-    const val versionName = "1.0.3"
+    const val versionCode = 5
+    const val versionName = "1.0.4"
 }
